7cd3c6fab4be5d4e5e35e301642a220b7fa09384
andrewotule <andrewotule2@gmail.com>
2022-07-21 09:56:33 +0300

    Sixth commit - Enabling firebase authentication for the Login and Register pages

--- app/src/main/java/com/andrew/bbcnewsapp/LoginActivity.kt
+++ app/src/main/java/com/andrew/bbcnewsapp/LoginActivity.kt
@@ -1,21 +1,90 @@
 package com.andrew.bbcnewsapp
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        //ASSIGNING TASK TO LOG IN BUTTON
-        val loginbtn = findViewById<Button>(R.id.button)
-        loginbtn.setOnClickListener{
-            val i = Intent(this, MainActivity::class.java)
-            startActivity(i)
+        auth = Firebase.auth
+
+        val loginbtn = findViewById<Button>(R.id.logbutton)
+        loginbtn.setOnClickListener {
+            performLogin()
         }
+
+        val registertext = findViewById<TextView>(R.id.goToRegister)
+        registertext.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        performLogin()
+    }
+    private fun performLogin() {
+        //Lets get input from the user
+        val email1: EditText = findViewById(R.id.log_email)
+        val password: EditText = findViewById(R.id.log_password)
+
+        //null checks on inputs
+        if (email1.text.isEmpty() || password.text.isEmpty()) {
+            Toast.makeText(this, "please fill all the fields", Toast.LENGTH_SHORT)
+                .show()
+            return
+        }else{
+            title = "Signing in"
+            val progressDialog = ProgressDialog(this)
+            progressDialog.setTitle("Signing in")
+            progressDialog.setMessage("Application is loading, please wait...")
+            progressDialog.show()
+        }
+
+        val emailinput = email1.text.toString()
+        val passwordinput = password.text.toString()
+
+        auth.signInWithEmailAndPassword(emailinput, passwordinput)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success,navigate to the main activity
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+
+                    Toast.makeText(
+                        baseContext, "Success.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                } else {
+                    // If sign in fails, display a message to the user.
+
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(
+                    baseContext, "Authentication failed.${it.localizedMessage}",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }
 
     }
 }
--- app/src/main/java/com/andrew/bbcnewsapp/RegisterActivity.kt
+++ app/src/main/java/com/andrew/bbcnewsapp/RegisterActivity.kt
@@ -1,11 +1,95 @@
 package com.andrew.bbcnewsapp
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        val signintext = findViewById<TextView>(R.id.goToSignIn)
+        signintext.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        val registerbtn = findViewById<Button>(R.id.regbutton)
+        registerbtn.setOnClickListener {
+            performSignUp()
+        }
+
+        //lets get the email and password for the user
+
+        performSignUp()
+    }
+
+    private fun performSignUp() {
+        val email = findViewById<EditText>(R.id.reg_email)
+        val password = findViewById<EditText>(R.id.reg_password)
+
+
+
+        if (email.text.isEmpty() || password.text.isEmpty()) {
+            Toast.makeText(this, "please fill all the fields", Toast.LENGTH_SHORT)
+                .show()
+            return
+
+        }else
+        {
+            title = "Signing Up"
+            val progressDialog = ProgressDialog(this)
+            progressDialog.setTitle("Signing Up")
+            progressDialog.setMessage("Application is loading, please wait...")
+            progressDialog.show()
+        }
+
+        val inputEmail = email.text.toString()
+        val inputPassword = password.text.toString()
+
+        auth.createUserWithEmailAndPassword(inputEmail,inputPassword)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, move to the next activity i.e Main activity
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+
+                    Toast.makeText(
+                        baseContext, "Successful login.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                } else {
+                    // If sign in fails, display a message to the user.
+
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(this,"Error occured ${it.localizedMessage}", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
     }
 }
